899dbea3c528b5dbfc1ef04c748264fdfb0aa537
domsenn <dom.senn@gmail.com>
2013-08-03 00:41:00 -0700

    fs.readFileSync auskommentiert

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index.html').toString());
+    #response.send(fs.readFileSync('index.html').toString()); 
+    response.send('Test');
 });
 
 var port = process.env.PORT || 5000;
