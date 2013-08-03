e99e0c637c8762721299f783032d79c531110b46
domsenn <dom.senn@gmail.com>
2013-08-03 01:03:07 -0700

    erneuter Versuch mit fs.readString

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    // response.send(fs.readFileSync('index.html').toString());
-    response.send('TEST');
+    response.send(fs.readFileSync('index.html').toString());
+    // fs.readFileSync('index.html').toString()
+    // response.send('TEST');
 });
 
 var port = process.env.PORT || 5000;
